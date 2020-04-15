12fc4d4adf1cd4ba4f76baa432d4dea6c1b9ab0b
Christophoros <mpekchri@auth.gr>
2020-04-15 20:46:01 +0300

    limited functionality (single get request to server) added in controller. It is accessible under the /basic route.

--- src/Controller/BasicController.php
+++ src/Controller/BasicController.php
@@ -12,9 +12,25 @@ class BasicController extends AbstractController
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BasicController.php',
-        ]);
+      $url = 'https://a831bqiv1d.execute-api.eu-west-1.amazonaws.com/dev/results';
+      $options = array(
+          'http' => array(
+              'header'  => "Content-type: application/json\r\n",
+              'method'  => 'GET',
+          )
+      );
+      $context  = stream_context_create($options);
+      $result = file_get_contents($url, false, $context);
+      if ($result === FALSE) {
+        /* Handle error */
+        $result = 'IT SHOULD RAISE AN EXCEPTION, SHOULDNT IT?';
+      }else{
+        $result = json_decode($result);
+      }
+      // \var_dump($result); die;
+
+      return $this->json([
+          'result' => $result,
+      ]);
     }
 }
