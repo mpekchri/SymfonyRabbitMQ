349704cba0779c1dc12083d343f6e684d39f0ec0
Christophoros <mpekchri@auth.gr>
2020-04-16 18:36:55 +0300

    configured the php-amqplib/rabbitmq-bundle properly, but still has BUGs. The bundle is no longer maintained, so I should follow different approach. The rabbitmq branch is no longer useful.

--- src/Service/MessageHandler.php
+++ src/Service/MessageHandler.php
@@ -3,6 +3,9 @@
 namespace App\Service;
 
 use App\Producer\MessageProducer;
+// require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
 class MessageHandler
 {
@@ -15,14 +18,26 @@ class MessageHandler
     $this->producer = $producer;
   }
 
-  public function publishMessage($serialized_data)
+  public function publishMessage($deserialized_data)
   {
-    $messageBody = $serialized_data['body'];
-    $routingKey = $serialized_data['key'];
-    // publish message to exchange
+    $messageBody = $deserialized_data['body'];
+    $routingKey = $deserialized_data['key'];
+
+    // // publish message to exchange
     $this->producer->setContentType('application/json');
-    // $this->producer->publish($msgBody [, $routingKey, $additionalProperties, $headers])
-    $this->producer->publish($messageBody, $routingKey);
+    // // $this->producer->publish($msgBody [, $routingKey, $additionalProperties, $headers])
+    $this->producer->publish(json_encode($messageBody), array(
+      'routingKey' => $routingKey
+    ));
+
+
+    // $connection = new AMQPStreamConnection('candidatemq.n2g-dev.net', 5672, 'cand_d4uf', 'Yd6bCNQgWpx429zr');
+    // $channel = $connection->channel();
+    // $msg = new AMQPMessage('Hello World!');
+    // $channel->basic_publish($msg, 'cand_d4uf', $routingKey);
+    // $channel->close();
+    // $connection->close();
+
   }
 
 
